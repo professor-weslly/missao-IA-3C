6cbf3f1707e8906a2f3e6c3098178d2612957a70
julijunkuhn <151845108+julijunkuhn@users.noreply.github.com>
2024-09-11 07:51:45 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Julia" , " Maria" , "Valentina" , "Murilo" , "Guilherme" , "Felipe"];
+
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.lenght);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes)
